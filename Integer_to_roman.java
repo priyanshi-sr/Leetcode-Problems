2cb74de4caf5e1b11334affc973fcd1d994131ee
Leetcode-Problems <priyanshisrivastava906@gmail.com>
2024-03-08 14:31:40 +0530

    Added one solution to the problem

---
+++ Integer_to_roman.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.Scanner;
+
+public class Integer_to_roman {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(romanToInt(num));
+    }
+
+    public static  String romanToInt(int num) {
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return roman.toString();
+    }
+}
